a47eb7d9f5725e0baba05bcb5c76b7cbf56e3a4d
BorislavMagaleas <95872181+BorislavMagaleas@users.noreply.github.com>
2022-01-03 13:54:26 +0200

    Two-dimensional right triangle.

---
+++ 2d-triangle.pattern.HW.py
@@ -0,0 +1,17 @@
+# HW3*: 
+# DRAW
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+print("\n")
+
+for k in range(1,6):
+    for x in range(k):
+        print("* ", end="")
+    print()
+        
+print("\n")
